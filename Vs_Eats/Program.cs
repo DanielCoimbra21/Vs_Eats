140e937dd249394809b767704df59ba4ad625b5b
Carlos Daniel Coimbra Soares <danielsoares321@hotmail.com>
2021-10-19 16:24:00 +0200

    Add project files.

---
+++ Vs_Eats/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vs_Eats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
