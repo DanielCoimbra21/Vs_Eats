a5141f2a7428f70a8b8da82b05489768af61e11a
Thomas Cheseaux <thomas.cheseaux@netplus.ch>
2021-12-27 14:32:39 +0100

    order cart

--- WebAppVSEAT/Controllers/RestaurantController.cs
+++ WebAppVSEAT/Controllers/RestaurantController.cs
@@ -20,7 +20,7 @@ namespace WebAppVSEAT.Controllers
         private List<DTO.Dish> ld = new List<DTO.Dish>();
 
 
-        public RestaurantController(ICityManager cityManager,IRestaurantManager restaurantManager, IDishManager dishManager, IDishesRestaurantManager dishesRestaurantManager)
+        public RestaurantController(ICityManager cityManager, IRestaurantManager restaurantManager, IDishManager dishManager, IDishesRestaurantManager dishesRestaurantManager)
         {
             RestaurantManager = restaurantManager;
             DishesRestaurantManager = dishesRestaurantManager;
@@ -28,10 +28,9 @@ namespace WebAppVSEAT.Controllers
             CityManager = cityManager;
         }
 
-        
+
         public IActionResult Index()
         {
-
             if (HttpContext.Session.GetInt32("IdCustomer") == null)
             {
                 return RedirectToAction("Index", "Login");
@@ -39,9 +38,9 @@ namespace WebAppVSEAT.Controllers
 
             var rest = RestaurantManager.GetRestaurants();
             var restaurants_vm = new List<RestaurantVM>();
-           
 
-            foreach(var r in rest)
+
+            foreach (var r in rest)
             {
                 var vm = new RestaurantVM();
                 var city = CityManager.GetCity(r.IDCITY);
@@ -86,31 +85,85 @@ namespace WebAppVSEAT.Controllers
                 results.Add(DishManager.GetDish(idDish));
             }
 
-            
+
 
             var vm = new DishesVM()
             {
                 Dishes = results
             };
-            
 
-             return View(vm);
+
+            return View(vm);
         }
 
-        public ActionResult Add(int id)
+        public IActionResult Details2(int id)
+        {
+
+            if (HttpContext.Session.GetInt32("IdCustomer") == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            var listDish = new List<int>();
+            listDish = DishesRestaurantManager.GetListDishes(id);
+            var dishesRestaurantVM = new List<DishesRestaurantVM>();
+
+
+            if (listDish == null)
+            {
+                var error = new ErrorViewModel();
+                return View(error);
+            }
+
+            foreach (var idDish in listDish)
+            {
+                var vm = new DishesRestaurantVM();
+                var dish = DishManager.GetDish(idDish);
+                vm.IDRESTAURANT = id;
+                vm.NAMEDISH = dish.NAMEDISH;
+                vm.PRICEDISH = dish.PRICEDISH;
+                vm.IDDISHES = idDish;
+
+                dishesRestaurantVM.Add(vm);
+            }
+       
+
+            return View(dishesRestaurantVM);
+        }
+
+        public IActionResult TakeAnOrder(int id)
         {
             if (HttpContext.Session.GetInt32("IdCustomer") == null)
             {
                 return RedirectToAction("Index", "Login");
             }
 
-            var dish = DishManager.GetDish(id);
-            return null;
+            var listDish = new List<int>();
+            listDish = DishesRestaurantManager.GetListDishes(id);
+            var commandVMs = new List<CommandVM>();
+
+
+            if (listDish == null)
+            {
+                var error = new ErrorViewModel();
+                return View(error);
+            }
+
+            foreach (var idDish in listDish)
+            {
+                var vm = new CommandVM();
+                var dish = DishManager.GetDish(idDish);
+                vm.NAMEDISH = dish.NAMEDISH;
+                vm.PRICEDISH = dish.PRICEDISH;
+                vm.QUANTITY = 0;
+               
+
+                commandVMs.Add(vm);
+            }
+
+
+            return View(commandVMs);
         }
-
-       
-       
-
 
     }
 }
--- WebAppVSEAT/Models/CommandVM.cs
+++ WebAppVSEAT/Models/CommandVM.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebAppVSEAT.Models
 {
-    public class DishesVM
+    public class CommandVM
     {
-        public int IDDISHES { get; set; }
         public string NAMEDISH { get; set; }
         public double PRICEDISH { get; set; }
-        public IEnumerable<DTO.Dish> Dishes { get; set; }
-    }
+        public int QUANTITY { get; set; }
 
-    
+       
+    }
 }
