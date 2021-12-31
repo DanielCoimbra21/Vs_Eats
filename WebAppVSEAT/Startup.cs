88ce1504cf88795510bdad32a42573d16180c843
Carlos Daniel Coimbra Soares <danielsoares321@hotmail.com>
2021-12-31 12:17:52 +0100

    Interface Graphique

--- WebAppVSEAT/Startup.cs
+++ WebAppVSEAT/Startup.cs
@@ -66,7 +66,9 @@ namespace WebAppVSEAT
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
+                //app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
