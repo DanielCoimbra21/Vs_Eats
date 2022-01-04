b6388866e1f4928dd8cc23da7c6a415cf6cca78f
Thomas Cheseaux <thomas.cheseaux@netplus.ch>
2022-01-04 21:19:07 +0100

    add search bar + menu home

---
+++ WebAppVSEAT/Controllers/HomePageController.cs
@@ -0,0 +1,56 @@
+﻿using BLL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppVSEAT.Models;
+
+namespace WebAppVSEAT.Controllers
+{
+    public class HomePageController : Controller
+    {
+        private IRestaurantManager RestaurantManager { get; }
+        private IDishesRestaurantManager DishesRestaurantManager { get; }
+        private IDishManager DishManager { get; }
+        private ICityManager CityManager { get; }
+        private IOrderManager OrderManager { get; }
+        private IDishesOrderManager DishesOrderManager { get; }
+
+        public HomePageController(IDishesOrderManager dishesOrderManager, IOrderManager orderManager, ICityManager cityManager, IRestaurantManager restaurantManager, IDishManager dishManager, IDishesRestaurantManager dishesRestaurantManager)
+        {
+            RestaurantManager = restaurantManager;
+            DishesRestaurantManager = dishesRestaurantManager;
+            DishManager = dishManager;
+            CityManager = cityManager;
+            OrderManager = orderManager;
+            DishesOrderManager = dishesOrderManager;
+        }
+
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetInt32("IdCustomer") == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            var rest = RestaurantManager.GetRestaurants();
+            var restaurants_vm = new List<RestaurantVM>();
+
+
+            foreach (var r in rest)
+            {
+                var vm = new RestaurantVM();
+                var city = CityManager.GetCity(r.IDCITY);
+                vm.CITYNAME = city.CITYNAME;
+                vm.IDRESTAURANT = r.IDRESTAURANT;
+                vm.NAMERESTAURANT = r.NAMERESTAURANT;
+                vm.ADDRESSRESTAURANT = r.ADDRESSRESTAURANT;
+
+                restaurants_vm.Add(vm);
+            }
+            return View(restaurants_vm);
+        }
+    }
+}
---
+++ WebAppVSEAT/Models/HomePageVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppVSEAT.Models
+{
+    public class HomePageVM
+    {
+        public int IDRESTAURANT { get; set; }
+        public string CITYNAME { get; set; }
+        public string NAMERESTAURANT { get; set; }
+        public string ADDRESSRESTAURANT { get; set; }
+
+        public int IDDISHES { get; set; }
+        public string NAMEDISH { get; set; }
+        public double PRICEDISH { get; set; }
+
+        public IEnumerable<DishesRestaurantVM> DishesR { get; set; }
+    }
+}
