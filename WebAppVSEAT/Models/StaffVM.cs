00d1a7ae45b9a4004fcd0cce6334bba4013f4747
Carlos Daniel Coimbra Soares <danielsoares321@hotmail.com>
2022-01-05 12:59:26 +0100

    Profiles and dropdown menu

--- WebAppVSEAT/Models/StaffVM.cs
+++ WebAppVSEAT/Models/StaffVM.cs
@@ -10,13 +10,23 @@ namespace WebAppVSEAT.Models
     {
         public int IDSTAFF { get; set; }
         public int IDCITY { get; set; }
+        [Required]
         public string CITYNAME { get; set; }
+        [Required]
         public string NAMESTAFF { get; set; }
+        [Required]
         public string SURNAMESTAFF { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
         public int PHONENUMBERSTAFF { get; set; }
+        [Required]
         public string ADDRESSSTAFF { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string MAILSTAFF { get; set; }
         public string USERNAMESTAFF { get; set; }
+        
+        [DataType(DataType.Password)]
         public string PASSWORDSTAFF { get; set; }
     }
 }
