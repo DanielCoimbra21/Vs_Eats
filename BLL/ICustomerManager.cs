f05f6abd4f193a7233b4f71c5df7e7a7a219d0ec
Carlos Daniel Coimbra Soares <danielsoares321@hotmail.com>
2022-01-08 14:40:13 +0100

    Hashing

--- BLL/ICustomerManager.cs
+++ BLL/ICustomerManager.cs
@@ -9,21 +9,30 @@ namespace BLL
 {
     public interface ICustomerManager
     {
-
-        public Customer GetCustomer(string email, string password);
+        public Customer GetCustomer(string email);
+        //public Customer GetCustomer(string email, string password);
 
         public List<Customer> GetCustomers();
         public void UpdatePassword(Customer customer);
 
         public void InsertCustomer(Customer customer);
 
-        public string GetPassword(string password);
 
         public string HashPassword(string password);
 
         public Customer GetCustomerID(int id);
 
         public void UpdateCustomer(Customer customer);
+
+        //public string HashPassword(string password, string salt);
+
+        public string SetPassword(string password);
+
+        public string GetPassword(string mail);
+
+
+        public Boolean VerifyPassword(string passwordLogin, string passwordStored);
+
 
     }
 
