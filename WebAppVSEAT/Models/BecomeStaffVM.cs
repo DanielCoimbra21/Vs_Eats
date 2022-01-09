d6f96f40598df2cf267ea00fc4ff95afa2503498
Carlos Daniel Coimbra Soares <danielsoares321@hotmail.com>
2022-01-09 17:26:17 +0100

    BecomeStaff Error

--- WebAppVSEAT/Models/BecomeStaffVM.cs
+++ WebAppVSEAT/Models/BecomeStaffVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,13 @@ namespace WebAppVSEAT.Models
 {
     public class BecomeStaffVM
     {
+     
+        [Required]
+        
         public string mailFrom { get; set;}
+        [Required]
         public string subjectMail { get; set; }
+        [Required]
         public string bodyMail { get; set; }
     }
 }
